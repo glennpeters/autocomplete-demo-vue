6bb4c24b42628aa60b67397f6f0533af44686809
Glenn Peters <glenn.b.peters@gmail.com>
2022-04-14 16:35:49 -0700

    Added loggedIn dot (class-based)

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -7,7 +7,7 @@
     <ul>
       <li><a @click.stop.prevent="clickSearch"><img src="@/assets/ic/white/ic_search.png" /></a></li>
       <li><a @click.stop.prevent="clickCart"><img src="@/assets/ic/white/ic_shopping_cart.png" /></a></li>
-      <li><a @click.stop.prevent="clickUser"><img src="@/assets/ic/white/ic_person.png" /></a></li>
+      <li><a @click.stop.prevent="clickUser" class="loggedIn"><img src="@/assets/ic/white/ic_person.png" /></a></li>
       <li><a @click.stop.prevent="clickMenu"><img src="@/assets/ic/white/ic_menu.png" /></a></li>
     </ul>
   </nav>
@@ -99,5 +99,26 @@ ul li {
 ul li img {
   display: inline-block;
 }
+
+.loggedIn {
+  position: relative;
+}
+
+.loggedIn:after {
+  content: '';
+  height: 6px;
+  width: 6px;
+  line-height: 6px;
+  border-radius: 50%;
+  background-color: #eebd31;
+  color: #eebd31;
+  text-align: center;
+  position: absolute;
+  top: -5px;
+  right: -6px;
+  /* I see a drop shadow in the comp, I'm not to to tweak it beyond this. */
+  box-shadow: inset -1px 3px 8px 5px #eebd31, 2px 5px 16px 0px #666, 5px 5px 15px 5px rgba(0,0,0,0);
+  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
+} 
 
 </style>
