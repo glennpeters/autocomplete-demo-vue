3d087c27d7628829487eadde3dc2e86a1c0784e8
Glenn Peters <glenn.b.peters@gmail.com>
2022-04-14 13:16:49 -0700

    Updated with API call to SWAPI, basic integration with autocomplete component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
     <SearchAutocomplete
-      :items="[
-        'Apple',
-        'Banana',
-        'Orange',
-        'Mango',
-        'Pear',
-        'Peach',
-        'Grape',
-        'Tangerine',
-        'Pineapple'
-      ]"
+      :items="this.info"
     />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 // import HelloWorld from './components/HelloWorld.vue'
 // import Home from './components/Home.vue';
 import SearchAutocomplete from './components/SearchAutocomplete.vue';
 
 export default {
   name: 'App',
+  data () {
+    return {
+      info: null
+    }
+  },
   components: {
     SearchAutocomplete
-  }
+  },
+  mounted () {
+    axios
+      .get('https://swapi.dev/api/people/')
+      // .then(response => (this.info = response))
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Success!  raw response.data = ", response.data);
+
+          // All sorts of error checking can be done here depending on level of trust 
+          this.info = (response.data.results).map(x => x.name);
+
+          // Note also this is returning just the first slice of names
+        }
+
+        console.log("Success!  this.info = ", this.info);
+      });
+  },  
 }
 </script>
 
